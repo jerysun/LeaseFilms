ef0546ecacd6a55df973a41d78e7a5c2c615abfa
Jerry Sun <jerysun007@hotmail.com>
2019-12-01 23:30:20 +0100

    First commit

---
+++ LeaseFilms/Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using LeaseFilms.Models;
+using LeaseFilms.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace LeaseFilms.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# LeaseFilms - Managing your movies rental business efficiently!
+
+LeaseFilms is an ASP.NET MVC open source project based on .NET Framework 4.7.2.
+
+## It has the following features:
+- Owin
+- Razor
+- Automapper
+- Entity Framework
+- Facebook account logon
+
+Have fun,
+
+Jerry Sun ( jerysun007@hotmail.com )
